a4d16feb336b7d64f3601b57e06b446599b37cc2
tushar deore <deoretushar10@gmail.com>
2019-11-25 14:57:00 +0000

    FirstCommit

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
